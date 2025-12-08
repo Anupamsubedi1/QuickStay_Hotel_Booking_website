4cbbcc9e954b9fcecbc1b8cd3a706651c26e8523
Anupamsubedi1 <anupamsubedi542@gmail.com>
2025-12-08 10:53:04 +0545

    featured hotel component and exclusive offer component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../component/Hero'
 import FeaturedDestination from '../component/FeaturedDestination'
+import ExclusiveOffer from '../component/ExclusiveOffer'
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedDestination />
+      <ExclusiveOffer />
     </>
   )
 }
