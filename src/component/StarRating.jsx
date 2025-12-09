4ad04e9a560237696f373399d12f1dca490a6ab1
Anupamsubedi1 <anupamsubedi542@gmail.com>
2025-12-09 18:29:23 +0545

    hover effect added for testimonial

--- src/component/StarRating.jsx
+++ src/component/StarRating.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import {assets} from '../assets/assets'
 
-const StarRating = () => {
+const StarRating = ({rating=4}) => {
   return (
-    <div>{Array(5).fill('').map((_, index) => (
-                                <img src={assets.starIconFilled} alt="" />
+    <div className="flex">{Array(5).fill('').map((_, index) => (
+                                <img src={rating > index ? assets.starIconFilled : assets.starIconOutlined} alt="staricon" className='w-4.5 h-4.5'/>
                             ))}</div>
   )
 }
