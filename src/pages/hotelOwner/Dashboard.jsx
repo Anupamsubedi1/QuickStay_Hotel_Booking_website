f9ad68ac228eb8403c08a4dfececd0e20cd5bf1e
Anupamsubedi1 <anupamsubedi542@gmail.com>
2025-12-17 21:38:07 +0545

    dashboard changed

--- src/pages/hotelOwner/Dashboard.jsx
+++ src/pages/hotelOwner/Dashboard.jsx
@@ -22,14 +22,25 @@ const Dashboard = () => {
                     alt=""
                     className='max-sm:hidden h-10'
                 />
-                <div>
-                    <p>Total Bookings</p>
-                    <p>{}</p>
+                <div className='flex flex-col sm:ml-4 font-medium'>
+                    <p className='text-blue-500 text-lg'>Total Bookings</p>
+                    <p className='text-neutral-400 text-base'>{dasboardData.totalBookings}</p>
+                </div>
+                </div><div className='bg-primary/3 border border-primary/10 rounded flex p-4 pr-8'>
+                <img
+                    src={assets.totalRevenueIcon}
+                    alt=""
+                    className='max-sm:hidden h-10'
+                />
+                <div className='flex flex-col sm:ml-4 font-medium'>
+                    <p className='text-blue-500 text-lg'>Total Revenue</p>
+                    <p className='text-neutral-400 text-base'>${dasboardData.totalRevenue}</p>
                 </div>
                 </div>
             </div>
 
             {/* ---- ---Total Revenue-- */}
+            
             <div>
   </div>
 </div>
