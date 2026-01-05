62f994b76f25b9e66fec5cfecebaa2df89410f25
Anupamsubedi1 <anupamsubedi542@gmail.com>
2026-01-05 18:05:12 +0545

    authmiddleware changed

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,15 +1,15 @@
 import User from '../models/User.js';
 
-// middleware to check if user is authenticated
-
 export const protect = async (req, res, next) => {
-    const {userId} = req.auth;
+    const { userId } = req.auth(); // ✅ FIX
+
     if (!userId) {
-        return res.status(401).json({ success :false ,message: 'Not authenticated' });
+        return res
+            .status(401)
+            .json({ success: false, message: 'Not authenticated' });
     }
-    else{
-        const user = await User.findById(userId);
-        req.user = user;
-        next();
-    }
-}
+
+    const user = await User.findById(userId);
+    req.user = user;
+    next();
+};
